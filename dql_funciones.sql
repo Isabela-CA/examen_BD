59f1079a080b4200fcc363d38cb8eaa25f93af68
isabela-ca <icarrilloazain@gmail.com>
2025-08-14 17:11:49 -0500

    feat: nuevas funciones

--- dql_funciones.sql
+++ dql_funciones.sql
@@ -47,3 +47,90 @@ where g.name = 'Pop';
  RETURN;
 END //
 DELIMITER ;
+
+-- 3. **PromedioPrecioPorAlbum(AlbumID)**: Retorna el precio promedio de las canciones de un álbum, -
+
+DELIMITER $$
+
+CREATE FUNCTION PromedioPrecioPorAlbum(p_AlbumID INT)
+RETURNS DECIMAL(10,2)
+DETERMINISTIC
+BEGIN
+    DECLARE promedio DECIMAL(10,2);
+
+    SELECT AVG(UnitPrice)
+    INTO promedio
+    FROM Track
+    WHERE AlbumId = p_AlbumID;
+
+    RETURN promedio;
+END $$
+
+DELIMITER ;
+
+-- 4. **DescuentoPorFrecuencia(ClienteID)**: Calcula el descuento a aplicar basado en la frecuencia de compra del cliente.
+DELIMITER $$
+
+CREATE FUNCTION DescuentoPorFrecuencia(p_ClienteID INT)
+RETURNS DECIMAL(5,2)
+DETERMINISTIC
+BEGIN
+    DECLARE totalCompras INT;
+    DECLARE descuento DECIMAL(5,2);
+
+    -- Contamos las compras del último año
+    SELECT COUNT(*) 
+    INTO totalCompras
+    FROM Invoice
+    WHERE CustomerId = p_ClienteID
+      AND InvoiceDate >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+
+    -- Asignamos descuento según frecuencia
+    IF totalCompras >= 20 THEN
+        SET descuento = 0.20; -- 20%
+    ELSEIF totalCompras >= 10 THEN
+        SET descuento = 0.10; -- 10%
+    ELSEIF totalCompras >= 5 THEN
+        SET descuento = 0.05; -- 5%
+    ELSE
+        SET descuento = 0.00; -- Sin descuento
+    END IF;
+
+    RETURN descuento;
+END $$
+
+DELIMITER ;
+
+
+
+-- 5. **VerificarClienteVIP(ClienteID)**: Verifica si un cliente es "VIP" basándose en sus gastos anuales.
+
+DELIMITER $$
+
+CREATE FUNCTION VerificarClienteVIP(p_ClienteID INT)
+RETURNS TINYINT
+DETERMINISTIC
+BEGIN
+    DECLARE gastoAnual DECIMAL(10,2);
+    DECLARE esVIP TINYINT;
+
+    -- Sumamos el total gastado por el cliente en el último año
+    SELECT SUM(Total)
+    INTO gastoAnual
+    FROM Invoice
+    WHERE CustomerId = p_ClienteID
+      AND InvoiceDate >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR);
+
+    -- Verificamos si supera el umbral
+    IF gastoAnual >= 100 THEN -- puedes ajustar el monto
+        SET esVIP = 1;
+    ELSE
+        SET esVIP = 0;
+    END IF;
+
+    RETURN esVIP;
+END $$
+
+DELIMITER ;
+
+
